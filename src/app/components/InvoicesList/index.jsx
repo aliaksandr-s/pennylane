4a7386b6636a05fe235d015eedadb7f78c7a5314
Alex S <s-a-n-9@mail.ru>
2021-10-26 12:25:04 +0200

    Add Invoices table

--- src/app/components/InvoicesList/index.jsx
+++ src/app/components/InvoicesList/index.jsx
@@ -1,81 +1,148 @@
 import { useApi } from "api";
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState, useMemo } from "react";
 import InfiniteScroll from 'react-infinite-scroller';
- 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import BTable from 'react-bootstrap/Table';
+import { useTable } from 'react-table';
+
+const Table = ({ columns, data }) => {
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  })
+
+  return (
+    <BTable bordered size="sm" {...getTableProps()}>
+      <thead class="sticky-header">
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+
+                console.log('Cell', cell.getCellProps())
+                return (
+                  <td {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </BTable>
+  )
+}
+
 const InvoicesList = () => {
-    const api = useApi();
-    const [invoicesList, setInvoicesList] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
+  const api = useApi();
+  const [invoicesList, setInvoicesList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-    const fetchInvoices = useCallback(async (nextPage, totalPages) => {
-        if (nextPage > totalPages) return;
+  const fetchInvoices = useCallback(async (nextPage, totalPages) => {
+    if (nextPage > totalPages) return;
 
-        try {
-            const { data } = await api.getInvoices({
-                per_page: 30,
-                page: nextPage,
-            });
+    try {
+      const { data } = await api.getInvoices({
+        per_page: 30,
+        page: nextPage,
+      });
 
-            setInvoicesList(invoicesList => [...invoicesList, ...data.invoices]);
-            setCurrentPage(data.pagination.page);
-            setTotalPages(data.pagination.total_pages);
-            console.log(data);
-        } catch (err) {
-            // could be a sentry call and notification for users in production;
-            console.log(err);
+      setInvoicesList(invoicesList => [...invoicesList, ...data.invoices]);
+      setCurrentPage(data.pagination.page);
+      setTotalPages(data.pagination.total_pages);
+      console.log(data);
+    } catch (err) {
+      // could be a sentry call and notification for users in production;
+      console.log(err);
+    }
+  }, [api]);
+
+  useEffect(() => {
+    fetchInvoices(1);
+  }, [fetchInvoices]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Id',
+        accessor: 'customer_id',
+      }, {
+        Header: 'Customer',
+        accessor: 'customer.first_name',
+      }, {
+        Header: 'Address',
+        accessor: 'customer.address',
+      }, {
+        Header: 'Total',
+        accessor: 'total',
+      }, {
+        Header: 'Tax',
+        accessor: 'tax',
+      }, {
+        Header: 'Finalized',
+        accessor: 'finalized',
+        Cell: ({ row }) => ( 
+          <span>
+            { row.original.finalized && 'Yes' }
+            { !row.original.finalized && 'No' }
+          </span>
+        ),
+      }, {
+        Header: 'Paid',
+        accessor: 'paid',
+        Cell: ({ row }) => ( 
+          <span>
+            { row.original.paid && 'Yes' }
+            { !row.original.paid && 'No' }
+          </span>
+        )
+      }, {
+        Header: 'Date',
+        accessor: 'date',
+      }, {
+        Header: 'Deadline',
+        accessor: 'deadline',
+      },{
+        Header: 'Actions',
+      },
+
+    ], [])
+
+  return (
+    <div style={{maxHeight: "100vh", overflowY: "scroll"}}>
+      <InfiniteScroll
+        initialLoad={true}
+        pageStart={0}
+        loadMore={() => fetchInvoices(currentPage + 1, totalPages)}
+        hasMore={currentPage < totalPages}
+        useWindow={false}
+        loader={
+          <div key="loading" className="loader">
+            Loading ...
+          </div>
         }
-    }, [api]);
-
-    useEffect(() => {
-        fetchInvoices(1);
-    }, [fetchInvoices]);
-
-    return (
-        <div style={{maxHeight: "100vh", overflowY: "scroll"}}>
-            <InfiniteScroll
-                initialLoad={true}
-                pageStart={0}
-                loadMore={() => fetchInvoices(currentPage + 1, totalPages)}
-                hasMore={currentPage < totalPages}
-                useWindow={false}
-                loader={
-                    <div key="loading" className="loader">
-                        Loading ...
-                    </div>
-                }
-            >
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Customer</th>
-                            <th>Address</th>
-                            <th>Total</th>
-                            <th>Tax</th>
-                            <th>Finalized</th>
-                            <th>Paid</th>
-                            <th>Date</th>
-                            <th>Deadline</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {invoicesList.map((invoice) => (<tr key={invoice.id}>
-                            <td>{invoice.id}</td>
-                            <td>{invoice.customer?.first_name} {invoice.customer?.last_name}</td>
-                            <td>{invoice.customer?.address}, {invoice.customer?.zip_code} {invoice.customer?.city}</td>
-                            <td>{invoice.total}</td>
-                            <td>{invoice.tax}</td>
-                            <td>{invoice.finalized ? "Yes" : "No"}</td>
-                            <td>{invoice.paid ? "Yes" : "No"}</td>
-                            <td>{invoice.date}</td>
-                            <td>{invoice.deadline}</td>
-                        </tr>))}
-                    </tbody>
-                </table>
-            </InfiniteScroll>
-        </div>
-    )
+      >
+        <Table columns={columns} data={invoicesList} />
+      </InfiniteScroll>
+    </div>
+  )
 }
 
 export default InvoicesList;
